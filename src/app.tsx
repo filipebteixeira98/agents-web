31af0a6a22c6d45d287f2cffdc1f41a159f0d186
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-07-06 02:40:08 -0300

    feat(routing): add room navigation pages

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import { CreateRoom } from './pages/create-room'
+import { Room } from './pages/room'
+
 export function App() {
-  return <div>Hello World</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<CreateRoom />} />
+        <Route element={<Room />} path="/room" />
+      </Routes>
+    </BrowserRouter>
+  )
 }

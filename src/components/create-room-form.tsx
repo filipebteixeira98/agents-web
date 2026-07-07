7e91fdb8e731abdb5ef691d31413fc8646918260
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-07-07 05:55:02 -0300

    fix(rooms): refresh list after room creation

--- src/components/create-room-form.tsx
+++ src/components/create-room-form.tsx
@@ -43,6 +43,8 @@ export function CreateRoomForm() {
 
   async function handleCreateRoom({ name, description }: CreateRoomFormData) {
     await createRoom({ name, description })
+
+    createRoomForm.reset()
   }
 
   return (
